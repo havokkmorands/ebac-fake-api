0cdfc7563e61bdb4b3c4875d58b76ff628634a55
Bruno Saldanha <brunosaldanhamarinho@gmail.com>
2025-12-23 16:49:14 -0300

    Atualizando API EBAC_SPORTS

--- src/pages/api/ebac_sports.ts
+++ src/pages/api/ebac_sports.ts
@@ -33,49 +33,49 @@ export default async function handler(
     {
         id: 1,
         nome: 'Bermuda Adidas Masculina',
-        imagem: 'https://ebac-fake-api.vercel.app/ebac_sports/bermuda.webp',
+        imagem: 'https://fake-api-havokk.vercel.app/ebac_sports/bermuda.webp',
         preco: 129.9
     },
     {
         id: 2,
         nome: 'Camisa Corinthians 1 - 22/23',
-        imagem: 'https://ebac-fake-api.vercel.app/ebac_sports/corinthians.webp',
+        imagem: 'https://fake-api-havokk.vercel.app/ebac_sports/corinthians.webp',
         preco: 249.9
     },
     {
         id: 3,
         nome: 'Bola de Vôlei Penalty',
-        imagem: 'https://ebac-fake-api.vercel.app/ebac_sports/bola.webp',
+        imagem: 'https://fake-api-havokk.vercel.app/ebac_sports/bola.webp',
         preco: 139.9
     },
     {
         id: 4,
         nome: 'Camisa Internacional 2 - 22/23',
-        imagem: 'https://ebac-fake-api.vercel.app/ebac_sports/internacional.webp',
+        imagem: 'https://fake-api-havokk.vercel.app/ebac_sports/internacional.webp',
         preco: 249.9
     },
     {
         id: 5,
         nome: 'Patins Preto',
-        imagem: 'https://ebac-fake-api.vercel.app/ebac_sports/patins.webp',
+        imagem: 'https://fake-api-havokk.vercel.app/ebac_sports/patins.webp',
         preco: 399.9
     },
     {
         id: 6, 
         nome: 'Camisa Kansas City Chiefs',
-        imagem: 'https://ebac-fake-api.vercel.app/ebac_sports/camisa_kansas.webp',
+        imagem: 'https://fake-api-havokk.vercel.app/ebac_sports/camisa_kansas.webp',
         preco: 379.9
     },
     {
         id: 7,
         nome: 'Camisa Real Madrid 1 - 22/23',
-        imagem: 'https://ebac-fake-api.vercel.app/ebac_sports/real_madrid.webp',
+        imagem: 'https://fake-api-havokk.vercel.app/ebac_sports/real_madrid.webp',
         preco: 349.9
     },
     {
         id: 8,
         nome: 'Camisa Milan 1 - 22/23',
-        imagem: 'https://ebac-fake-api.vercel.app/ebac_sports/milan.webp',
+        imagem: 'https://fake-api-havokk.vercel.app/ebac_sports/milan.webp',
         preco: 349.9
     },
   ])
